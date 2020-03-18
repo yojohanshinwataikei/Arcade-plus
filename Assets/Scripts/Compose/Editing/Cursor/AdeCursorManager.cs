17388e244a076f8db14164a2d15b04b097fef7c3
yojohanshinwataikei <yojohanshinwataikei@yandex.com>
2020-03-18 18:29:27 +0800

    Cursor Info should use 2 digits position

--- Assets/Scripts/Compose/Editing/Cursor/AdeCursorManager.cs
+++ Assets/Scripts/Compose/Editing/Cursor/AdeCursorManager.cs
@@ -357,7 +357,7 @@ namespace Arcade.Compose
 			if (EnableVertical)
 			{
 				Vector3 pos = AttachedVerticalPoint;
-				content += $"\n坐标: ({ArcAlgorithm.WorldXToArc(pos.x).ToString("f3")},{ArcAlgorithm.WorldYToArc(pos.y).ToString("f3")})";
+				content += $"\n坐标: ({ArcAlgorithm.WorldXToArc(pos.x).ToString("f2")},{ArcAlgorithm.WorldYToArc(pos.y).ToString("f2")})";
 			}
 			if (AdeClickToCreate.Instance.Enable && AdeClickToCreate.Instance.Mode != ClickToCreateMode.Idle)
 			{
@@ -375,7 +375,7 @@ namespace Arcade.Compose
 				{
 					float x = ArcAlgorithm.X(arc.XStart, arc.XEnd, p, arc.LineType);
 					float y = ArcAlgorithm.Y(arc.YStart, arc.YEnd, p, arc.LineType);
-					content += $"\nArc: {(p * 100).ToString("f2")}%, {x.ToString("f3")}, {y.ToString("f3")}";
+					content += $"\nArc: {(p * 100).ToString("f2")}%, {x.ToString("f2")}, {y.ToString("f2")}";
 				}
 			}
 			InfoText.text = content;
