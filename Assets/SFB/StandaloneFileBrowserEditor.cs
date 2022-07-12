a3eef3435aa2a36fe535d7ae2469f4ba171fa84c
Linwenxuan04 <llwwxxllwwxx@gmail.com>
2022-07-13 01:21:16 +0800

    Fixed Setting of SFB
    Enabled SFB to work in macOS correctly through change setting
    
    Refactored coding style

--- Assets/SFB/StandaloneFileBrowserEditor.cs
+++ Assets/SFB/StandaloneFileBrowserEditor.cs
@@ -3,33 +3,32 @@
 using System;
 using UnityEditor;
 
-namespace SFB {
-    public class StandaloneFileBrowserEditor : IStandaloneFileBrowser  {
-        public string[] OpenFilePanel(string title, string directory, ExtensionFilter[] extensions, bool multiselect) {
+namespace SFB 
+{
+    public class StandaloneFileBrowserEditor : IStandaloneFileBrowser  
+    {
+        public string[] OpenFilePanel(string title, string directory, ExtensionFilter[] extensions, bool multiselect) 
+        {
             string path = "";
 
-            if (extensions == null) {
-                path = EditorUtility.OpenFilePanel(title, directory, "");
-            }
-            else {
-                path = EditorUtility.OpenFilePanelWithFilters(title, directory, GetFilterFromFileExtensionList(extensions));
-            }
+            path = extensions == null 
+                ? EditorUtility.OpenFilePanel(title, directory, "") 
+                : EditorUtility.OpenFilePanelWithFilters(title, directory, GetFilterFromFileExtensionList(extensions));
 
-            return string.IsNullOrEmpty(path) ? new string[0] : new[] { path };
+            return string.IsNullOrEmpty(path) ? Array.Empty<string>() : new[] { path };
         }
 
-        public void OpenFilePanelAsync(string title, string directory, ExtensionFilter[] extensions, bool multiselect, Action<string[]> cb) {
+        public void OpenFilePanelAsync(string title, string directory, ExtensionFilter[] extensions, bool multiselect, Action<string[]> cb) => 
             cb.Invoke(OpenFilePanel(title, directory, extensions, multiselect));
-        }
 
-        public string[] OpenFolderPanel(string title, string directory, bool multiselect) {
+        public string[] OpenFolderPanel(string title, string directory, bool multiselect) 
+        {
             var path = EditorUtility.OpenFolderPanel(title, directory, "");
-            return string.IsNullOrEmpty(path) ? new string[0] : new[] {path};
+            return string.IsNullOrEmpty(path) ? Array.Empty<string>() : new[] {path};
         }
 
-        public void OpenFolderPanelAsync(string title, string directory, bool multiselect, Action<string[]> cb) {
+        public void OpenFolderPanelAsync(string title, string directory, bool multiselect, Action<string[]> cb) => 
             cb.Invoke(OpenFolderPanel(title, directory, multiselect));
-        }
 
         public string SaveFilePanel(string title, string directory, string defaultName, ExtensionFilter[] extensions) {
             var ext = extensions != null ? extensions[0].Extensions[0] : "";
@@ -37,16 +36,17 @@ namespace SFB {
             return EditorUtility.SaveFilePanel(title, directory, name, ext);
         }
 
-        public void SaveFilePanelAsync(string title, string directory, string defaultName, ExtensionFilter[] extensions, Action<string> cb) {
+        public void SaveFilePanelAsync(string title, string directory, string defaultName, ExtensionFilter[] extensions, Action<string> cb) => 
             cb.Invoke(SaveFilePanel(title, directory, defaultName, extensions));
-        }
 
         // EditorUtility.OpenFilePanelWithFilters extension filter format
-        private static string[] GetFilterFromFileExtensionList(ExtensionFilter[] extensions) {
+        private static string[] GetFilterFromFileExtensionList(ExtensionFilter[] extensions) 
+        {
             var filters = new string[extensions.Length * 2];
-            for (int i = 0; i < extensions.Length; i++) {
-                filters[(i * 2)] = extensions[i].Name;
-                filters[(i * 2) + 1] = string.Join(",", extensions[i].Extensions);
+            for (int i = 0; i < extensions.Length; i++) 
+            {
+                filters[i * 2] = extensions[i].Name;
+                filters[i * 2 + 1] = string.Join(",", extensions[i].Extensions);
             }
             return filters;
         }
--- Assets/SFB/StandaloneFileBrowserLinux.cs
+++ Assets/SFB/StandaloneFileBrowserLinux.cs
@@ -5,9 +5,11 @@ using System.IO;
 using System.Runtime.InteropServices;
 using UnityEngine;
 
-namespace SFB {
+namespace SFB 
+{
 
-    public class StandaloneFileBrowserLinux : IStandaloneFileBrowser {
+    public class StandaloneFileBrowserLinux : IStandaloneFileBrowser 
+    {
         
         private static Action<string[]> _openFileCb;
         private static Action<string[]> _openFolderCb;
@@ -18,25 +20,29 @@ namespace SFB {
 
         [DllImport("StandaloneFileBrowser")]
         private static extern void DialogInit();
+
         [DllImport("StandaloneFileBrowser")]
         private static extern IntPtr DialogOpenFilePanel(string title, string directory, string extension, bool multiselect);
+
         [DllImport("StandaloneFileBrowser")]
         private static extern void DialogOpenFilePanelAsync(string title, string directory, string extension, bool multiselect, AsyncCallback callback);
+
         [DllImport("StandaloneFileBrowser")]
         private static extern IntPtr DialogOpenFolderPanel(string title, string directory, bool multiselect);
+
         [DllImport("StandaloneFileBrowser")]
         private static extern void DialogOpenFolderPanelAsync(string title, string directory, bool multiselect, AsyncCallback callback);
+
         [DllImport("StandaloneFileBrowser")]
         private static extern IntPtr DialogSaveFilePanel(string title, string directory, string defaultName, string extension);
+
         [DllImport("StandaloneFileBrowser")]
         private static extern void DialogSaveFilePanelAsync(string title, string directory, string defaultName, string extension, AsyncCallback callback);
 
-        public StandaloneFileBrowserLinux()
-        {
-            DialogInit();
-        }
+        public StandaloneFileBrowserLinux() => DialogInit();
 
-        public string[] OpenFilePanel(string title, string directory, ExtensionFilter[] extensions, bool multiselect) {
+        public string[] OpenFilePanel(string title, string directory, ExtensionFilter[] extensions, bool multiselect) 
+        {
             var paths = Marshal.PtrToStringAnsi(DialogOpenFilePanel(
                 title,
                 directory,
@@ -45,7 +51,8 @@ namespace SFB {
             return paths.Split((char)28);
         }
 
-        public void OpenFilePanelAsync(string title, string directory, ExtensionFilter[] extensions, bool multiselect, Action<string[]> cb) {
+        public void OpenFilePanelAsync(string title, string directory, ExtensionFilter[] extensions, bool multiselect, Action<string[]> cb) 
+        {
             _openFileCb = cb;
             DialogOpenFilePanelAsync(
                 title,
@@ -55,7 +62,8 @@ namespace SFB {
                 (string result) => { _openFileCb.Invoke(result.Split((char)28)); });
         }
 
-        public string[] OpenFolderPanel(string title, string directory, bool multiselect) {
+        public string[] OpenFolderPanel(string title, string directory, bool multiselect) 
+        {
             var paths = Marshal.PtrToStringAnsi(DialogOpenFolderPanel(
                 title,
                 directory,
@@ -63,7 +71,8 @@ namespace SFB {
             return paths.Split((char)28);
         }
 
-        public void OpenFolderPanelAsync(string title, string directory, bool multiselect, Action<string[]> cb) {
+        public void OpenFolderPanelAsync(string title, string directory, bool multiselect, Action<string[]> cb) 
+        {
             _openFolderCb = cb;
             DialogOpenFolderPanelAsync(
                 title,
@@ -72,7 +81,8 @@ namespace SFB {
                 (string result) => { _openFolderCb.Invoke(result.Split((char)28)); });
         }
 
-        public string SaveFilePanel(string title, string directory, string defaultName, ExtensionFilter[] extensions) {
+        public string SaveFilePanel(string title, string directory, string defaultName, ExtensionFilter[] extensions) 
+        {
             return Marshal.PtrToStringAnsi(DialogSaveFilePanel(
                 title,
                 directory,
@@ -80,7 +90,8 @@ namespace SFB {
                 GetFilterFromFileExtensionList(extensions)));
         }
 
-        public void SaveFilePanelAsync(string title, string directory, string defaultName, ExtensionFilter[] extensions, Action<string> cb) {
+        public void SaveFilePanelAsync(string title, string directory, string defaultName, ExtensionFilter[] extensions, Action<string> cb) 
+        {
             _saveFileCb = cb;
             DialogSaveFilePanelAsync(
                 title,
@@ -90,18 +101,16 @@ namespace SFB {
                 (string result) => { _saveFileCb.Invoke(result); });
         }
 
-        private static string GetFilterFromFileExtensionList(ExtensionFilter[] extensions) {
-            if (extensions == null) {
-                return "";
-            }
+        private static string GetFilterFromFileExtensionList(ExtensionFilter[] extensions) 
+        {
+            if (extensions == null) return "";
 
             var filterString = "";
-            foreach (var filter in extensions) {
+            foreach (var filter in extensions) 
+            {
                 filterString += filter.Name + ";";
 
-                foreach (var ext in filter.Extensions) {
-                    filterString += ext + ",";
-                }
+                foreach (var ext in filter.Extensions) filterString += ext + ",";
 
                 filterString = filterString.Remove(filterString.Length - 1);
                 filterString += "|";
